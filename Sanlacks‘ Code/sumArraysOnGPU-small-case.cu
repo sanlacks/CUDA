ca6f8c8a145e5843d8bd579d9b883efc5ee63093
sanlacks <sanlacks@outlook.com>
2023-08-10 16:50:23 +0800

    Update sumArraysOnGPU-small-case.cu

--- Sanlacks‘ Code/sumArraysOnGPU-small-case.cu
+++ Sanlacks‘ Code/sumArraysOnGPU-small-case.cu
@@ -1,17 +1,15 @@
 #include "stdio.h"
 #include "cuda_runtime.h"
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
-#define CHECK(call){
-	const cudaError_t error = call;
-	if (error != cudaSuccess)
-	{
-		printf("Error: %s: %d, ", __FILE__, __LINE__);
-		printf("cuda: %d, reason: %s\n", error, cudaGetErrorString(error));
-		exit(1);
-	}
+
+#define CHECK(call){									\
+	const cudaError_t error = call;							\
+	if (error != cudaSuccess)							\
+	{										\
+		printf("Error: %s: %d, ", __FILE__, __LINE__);				\
+		printf("cuda: %d, reason: %s\n", error, cudaGetErrorString(error));	\
+		exit(1);								\
+	}										\
 }
 
 void checkResult(float* hostRef, float* gpuRef, const int N)
