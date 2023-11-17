ff9ea7d0fe099f341a69bce3e17a69f2da8efbe1
Sanlacks <Sanlacks@outlook.com>
2023-11-17 10:40:41 +0800

    Revert "1117"
    This reverts commit ba53385919978194eea8bbd7f60441799b4cd0b0.

--- CudaRuntime1/movingAverage.hpp
+++
@@ -1,6 +0,0 @@
-// moving_average.h
-#pragma once
-
-#include <vector>
-
-std::vector<float> movingAverage(const std::vector<float>& data, int windowSize);
--- CudaRuntime1/readCSV.hpp
+++
@@ -1,8 +0,0 @@
-// csv_reader.h
-#pragma once
-
-#include <vector>
-#include <fstream>
-#include <string>
-
-void readCSV(std::vector<float>& hostData, std::ifstream& file, int& lineCount);
